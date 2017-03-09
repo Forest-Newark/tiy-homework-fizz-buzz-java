9d5c953222ea06c0f45de3ecef569a2f3138f5dd
Forest Newark <forest.newark@gmail.com>
2017-03-09 15:39:24 -0500

    Added comments

--- src/main/java/com/ForestNewark/FizzBuzz/FizzBuzz.java
+++ src/main/java/com/ForestNewark/FizzBuzz/FizzBuzz.java
@@ -4,17 +4,33 @@ package com.ForestNewark.FizzBuzz;
  * Created by forestnewark on 3/9/17.
  */
 public class FizzBuzz {
+    /**
+     * This method is used to print out a list of integers from a user supplied
+     * starting point and ending point.
+     * This loop will replace every number that is a multiple of 3 with the String "Fizz".
+     * It will replace every multiple of 5 with the String "Buzz".
+     * It will replace numbers that are multiples of both 3 and 5 with the String "FizzBuzz".
+     *
+     * @param start provides starting point for loop
+     * @param end   provides ending point for loop
+     */
     public static void doIt(int start, int end) {
+        //Create a for loop that starts at "start" -int x = start; and will end at "end"
         for (int x = start; x <= end; x++) {
-            if(x % 3 == 0 && x % 5 == 0) {
+            //First conditional (if) checks to see if a # is a multiple of 3 AND 5
+            if (x % 3 == 0 && x % 5 == 0) {
                 System.out.println("FizzBuzz");
             }
-            else if(x % 3 == 0) {
+            //Else if statement checks to see if # is a multiple of 3
+            else if (x % 3 == 0) {
                 System.out.println("Fizz");
             }
-            else if(x % 5 == 0) {
+            //Else if statment checks to see if # is a multiple of 5
+            else if (x % 5 == 0) {
                 System.out.println("Buzz");
-            }else {
+            }
+            //Else statment prints out the current number (x) only if the number has failed the conditional test above.
+            else {
                 System.out.println(x);
             }
         }
--- src/main/java/com/ForestNewark/FizzBuzz/Main.java
+++ src/main/java/com/ForestNewark/FizzBuzz/Main.java
@@ -1,13 +1,14 @@
 package com.ForestNewark.FizzBuzz;
 
 /**
- * Hello world!
+ *This main class calls one Method "doIt" which belongs to the FizzBuzz Class.
  *
  */
 public class Main
 {
     public static void main( String[] args )
     {
+        //Call doIt on the FizzBuzz class and supply two int values "1" and "100".
         FizzBuzz.doIt(1,100);
 
     }
